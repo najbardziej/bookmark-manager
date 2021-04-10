8c45eb00c228656fc0995e8050ce8e69b4c89b21
Dangermannn <dangermanplyt@gmail.com>
2021-04-11 00:24:49 +0200

    Prepared model, dts and DataContext for authentication

---
+++ WebAPI/bookmark-manager.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using bookmark_manager.API.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace bookmark_manager.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ WebAPI/bookmark-manager.API/Entities/User.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace bookmark_manager.API.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
