a63796aaf7d5fa62d0d6f4c7a030ddb57e1024b7
Remigiusz Najbar <muffespl@gmail.com>
2021-04-10 20:54:09 +0200

    Added base angular template, added MDBootstrap dependencies

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModule, IconsModule } from 'angular-bootstrap-md';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IconsModule,
+    MDBBootstrapModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
