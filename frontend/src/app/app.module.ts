203e076ae9c112bc5ae7db0f53a9f5445dae71ab
pawel <pawel850m@gmail.com>
2021-04-13 11:43:09 +0200

    fix: add missing imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MDBBootstrapModule, IconsModule } from 'angular-bootstrap-md';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { FormsModule } from '@angular/forms';
+import { NgxWebstorageModule } from 'ngx-webstorage';
+import {JwtInterceptor} from './interceptor/JwtInterceptor';
 
 @NgModule({
   declarations: [
@@ -17,11 +20,17 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     IconsModule,
     MDBBootstrapModule.forRoot(),
+    NgxWebstorageModule.forRoot(),
     FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: JwtInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
