cce001914d84d100706f6d49a795fff8c3bb4069
Paweł Mąsior <62357939+pmasior@users.noreply.github.com>
2021-05-09 22:27:26 +0200

    CU-hv4xa3 - Add bookmark component

---
+++ frontend/src/app/bookmarks/bookmarks-list/convert-url-to-domain.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ConvertUrlToDomainPipe } from './convert-url-to-domain.pipe';
+
+describe('ConvertUrlToDomainPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ConvertUrlToDomainPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- frontend/src/app/bookmarks/bookmarks.module.ts
+++ frontend/src/app/bookmarks/bookmarks.module.ts
@@ -4,21 +4,21 @@ import { RouterModule } from '@angular/router';
 import { MDBBootstrapModule} from 'angular-bootstrap-md';
 import { BookmarksComponent } from './bookmarks.component';
 import { BookmarksListComponent } from './bookmarks-list/bookmarks-list.component';
-// import { ConvertUrlToDomainPipe } from './bookmarks-list/convert-url-to-domain.pipe';
+import { BookmarkComponent } from './bookmarks-list/bookmark/bookmark.component';
+import { ConvertUrlToDomainPipe } from './bookmarks-list/convert-url-to-domain.pipe';
 // import { SidebarComponent } from './sidebar/sidebar.component';
 // import { FolderListComponent } from './sidebar/folder-list/folder-list.component';
 // import { TagListComponent } from './sidebar/tag-list/tag-list.component';
-// import {BookmarkComponent} from './bookmarks-list/bookmark/bookmark.component';
 
 @NgModule({
     declarations: [
         BookmarksComponent,
         BookmarksListComponent,
-        // ConvertUrlToDomainPipe,
+        BookmarkComponent,
+        ConvertUrlToDomainPipe,
         // SidebarComponent,
         // FolderListComponent,
         // TagListComponent,
-        // BookmarkComponent,
     ],
   imports: [
     CommonModule,
