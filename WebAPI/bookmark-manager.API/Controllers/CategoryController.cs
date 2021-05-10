dc02b21b13c0be746e7fe914b191fe2b96af6334
Dangermannn <dangermanplyt@gmail.com>
2021-05-10 22:46:07 +0200

    Fixed NullReferenceException

--- WebAPI/bookmark-manager.API/Controllers/CategoryController.cs
+++ WebAPI/bookmark-manager.API/Controllers/CategoryController.cs
@@ -67,7 +67,7 @@ namespace bookmark_manager.API.Controllers
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserId == userId);
 
 
-            if(category.NodeLevel >= _MAX_NESTING)
+            if(category?.NodeLevel >= _MAX_NESTING)
                 return BadRequest($"Nest level cannot be higher than { _MAX_NESTING }");
             
             var categoryToAdd = new Category
