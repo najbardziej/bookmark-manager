54b7ae1365161c89f155613a1829624e0eb56e71
Dydek123 <mazurkiewicz707@gmail.com>
2021-05-18 18:22:36 +0200

    Update API urls to login and register

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { User } from '../model/user';
@@ -14,10 +15,10 @@ export class AuthService{
   constructor(private httpClient: HttpClient) {
   }
   login(model: User): Observable<User>{
-    return this.saveUser(model, 'account/login');
+    return this.saveUser(model, 'Account/login');
   }
   register(model: User): Observable<User>{
-    return this.saveUser(model, 'account/register');
+    return this.saveUser(model, 'Account/register');
   }
   private saveUser(model: User, url: string): Observable<User>{
     return this.httpClient.post<User>(this.baseUrl + url, model).pipe(
