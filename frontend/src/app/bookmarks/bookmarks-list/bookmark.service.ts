dabfbf53755d305f31f5bcbe7cfdff3db2f840db
Dydek123 <mazurkiewicz707@gmail.com>
2021-05-18 18:28:07 +0200

    Get bookmarks data from external API

--- frontend/src/app/bookmarks/bookmarks-list/bookmark.service.ts
+++ frontend/src/app/bookmarks/bookmarks-list/bookmark.service.ts
@@ -8,7 +8,7 @@ import {catchError, tap} from 'rxjs/operators';
   providedIn: 'root'
 })
 export class BookmarkService {
-  private bookmarkUrl = 'api/Bookmark/bookmarks.json';
+  private bookmarkUrl = 'http://localhost:8080/Bookmark/1';
 
   constructor(private http: HttpClient) { }
 
