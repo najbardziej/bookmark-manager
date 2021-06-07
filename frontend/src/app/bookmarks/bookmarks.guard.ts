7481ccc8eaa3fb5dc5520c18c8af52bc28a1876f
Dangermannn <dangermanplyt@gmail.com>
2021-06-07 22:06:47 +0200

    Added auth guard

--- frontend/src/app/bookmarks/bookmarks.guard.ts
+++ frontend/src/app/bookmarks/bookmarks.guard.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookmarksGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    // TODO: return true if user logged in, else return false
+
+  constructor(private router: Router){}
+
+  canActivate(): boolean {
+    if (localStorage.getItem('user') !== null) {
+      this.router.navigate(['/signin']);
+    }
     return true;
   }
 
