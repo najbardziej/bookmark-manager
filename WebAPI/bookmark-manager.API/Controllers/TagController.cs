b8e6826e0a6341a6f0c52f332b39e68304c87327
Dangermannn <dangermanplyt@gmail.com>
2021-06-08 21:18:22 +0200

    Prevent against updating tag to existing name

--- WebAPI/bookmark-manager.API/Controllers/TagController.cs
+++ WebAPI/bookmark-manager.API/Controllers/TagController.cs
@@ -78,7 +78,10 @@ namespace bookmark_manager.API.Controllers
         [HttpPut("{tagId}")]
         public async Task<ActionResult> UpdateCategory( int tagId, TagDto tagDto)
         {
+            var tagExists = await _context.Tags.SingleOrDefaultAsync(t => t.Name.Equals(tagDto.Name));
 
+            if(tagExists != null)
+                return BadRequest("Tag with that name already exists");
             var tag = await _context.Tags.Include(t => t.User).SingleOrDefaultAsync(t => t.Id == tagId &&
                                                         t.User.UserId == int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value));
 
